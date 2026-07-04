85a98b3c3304e7f9811cbc4f120a7d0bef0a7817
ash-hun <diadiahun0902@gmail.com>
2026-07-04 18:33:46 +0900

    update: katex"

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   site: 'https://ash-hun.github.io',
@@ -14,6 +16,8 @@ export default defineConfig({
     tailwind({ applyBaseStyles: false }),
   ],
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
     shikiConfig: {
       theme: 'github-dark',
       wrap: false,
